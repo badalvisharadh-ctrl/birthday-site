6d40e5c30f12f96dcfee3a0b182b661f617a3068
badalvisharadh-ctrl <badalvisharadh@gmail.com>
2026-02-06 18:10:21 +0530

    Refactor section navigation and add music toggle

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
-let current = 1;
-
-function nextSection() {
-  document.getElementById("sec" + current).classList.remove("active");
-  current++;
-  document.getElementById("sec" + current).classList.add("active");
+function go(current, next) {
+  document.getElementById(current).classList.remove("show");
+  document.getElementById(next).classList.add("show");
 }
 
-function revealFinal() {
+function reveal() {
   document.getElementById("finalImg").classList.remove("hidden");
-  document.getElementById("finalText").classList.remove("hidden");
-  nextSection();
+  document.getElementById("wish").classList.remove("hidden");
+  go("s11", "s12");
 }
 
 function showVideo() {
   document.getElementById("videoBox").classList.remove("hidden");
 }
+
+let playing = false;
+function toggleMusic() {
+  const music = document.getElementById("bgMusic");
+  if (!playing) {
+    music.play();
+  } else {
+    music.pause();
+  }
+  playing = !playing;
+}
+
 

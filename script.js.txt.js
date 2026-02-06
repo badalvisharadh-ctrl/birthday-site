e67d8c5c571ab74f35a4812a7c77a386815d4dc5
badalvisharadh-ctrl <badalvisharadh@gmail.com>
2026-02-06 16:55:25 +0530

    Add files via upload

---
+++ script.js.txt.js
@@ -0,0 +1,17 @@
+let current = 1;
+
+function nextSection() {
+  document.getElementById("sec" + current).classList.remove("active");
+  current++;
+  document.getElementById("sec" + current).classList.add("active");
+}
+
+function revealFinal() {
+  document.getElementById("finalImg").classList.remove("hidden");
+  document.getElementById("finalText").classList.remove("hidden");
+  nextSection();
+}
+
+function showVideo() {
+  document.getElementById("videoBox").classList.remove("hidden");
+}
